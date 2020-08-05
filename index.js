a99feee93a2eafeae59ae338090d735c2ebe8847
Elijah wines <elijahwines84@yahoo.com>
2020-08-05 16:01:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 // Code your solutions in this file
 function writeCards(names, event) {
-    const output = [];
-    for (let i = 0; i < names.length; i++) {
-        output.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
-    }
+  const output = [];
+  for (let i = 0; i < names.length; i++) {
+      output.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+  }
   return output;
 }
